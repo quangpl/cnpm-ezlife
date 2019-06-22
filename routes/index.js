730759bfb2a760a16df92f83af92811c57e66498
Phan Quang <phanlequang99@gmail.com>
2019-06-22 22:31:13 +0700

    Add : Combine front-end with express

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/admin', function(req, res, next) {
+  res.render('admin/index');
+});
+
 module.exports = router;
