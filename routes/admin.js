f3b554e2bd195e7ef7d4a0c331d50e02a3c6755f
Nguyễn Đức Hưng <dhungst1@gmail.com>
2019-06-26 19:07:00 +0700

    Edit: Admin views and some routes

--- routes/admin.js
+++ routes/admin.js
@@ -1,9 +1,37 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
+
 router.get('/', function(req, res, next) {
-    res.send('respond with a resource');
+    res.render('admin');
+});
+
+router.get('/bill', function(req, res, next) {
+    res.render('admin/bill_manager');
+});
+
+router.get('/customer', function(req, res, next) {
+    res.render('admin/customer_manager');
+});
+
+router.get('/book', function(req, res, next) {
+    res.render('admin/book_manager');
+});
+
+router.get('/staff', function(req, res, next) {
+    res.render('admin/staff_manager');
+});
+
+router.get('/report', function(req, res, next) {
+    res.render('admin/report');
+});
+
+router.get('/setting', function(req, res, next) {
+    res.render('admin/setting');
+});
+
+router.get('/login', function(req, res, next) {
+    res.render('admin/login');
 });
 
 module.exports = router;
--- routes/page.js
+++ routes/page.js
@@ -6,4 +6,32 @@ router.get('/', function(req, res, next) {
     res.render('./pages/index');
 });
 
+router.get('/all', function(req, res, next) {
+    res.render('./pages/all-products');
+});
+
+router.get('/cart', function(req, res, next) {
+    res.render('./pages/cart');
+});
+
+router.get('/checkout', function(req, res, next) {
+    res.render('./pages/checkout');
+});
+
+router.get('/login', function(req, res, next) {
+    res.render('./pages/login');
+});
+
+router.get('/my-account', function(req, res, next) {
+    res.render('./pages/my-account');
+});
+
+router.get('/product', function(req, res, next) {
+    res.render('./pages/single-product');
+});
+
+router.get('/wishlist', function(req, res, next) {
+    res.render('./pages/wishlist');
+});
+
 module.exports = router;
